668def7e10390045e1000dbe746d7e7d785a59f6
gautamvenkatesh <vegautam@gmail.com>
2021-02-21 16:35:34 -0800

    react added

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,17 @@
 const app = require('express')()
-const http = require('http').createServer()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http);
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://orangatan:taboo@cluster0.aeoy1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+
+io.on('connection', (socket) => {
+    socket.on('create', (data) => {
+        var code = Math.random() * 10000
+        socket.join(code)
+    })
+})
+
+
+http.listen(3000, () => {
+    console.log('listening on port 3000')
+})
